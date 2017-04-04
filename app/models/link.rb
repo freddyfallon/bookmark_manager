6324ff82b52d75da03dc256c9e14a8a1b0777e7a
Freddy Fallon <freddyfallon@gmail.com>
2017-04-04 17:35:08 +0100

    Add spaced to link

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,4 @@
+
 require 'dm-migrations'
 require 'data_mapper'
 require 'dm-postgres-adapter'
@@ -13,5 +14,7 @@ class Link
 end
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+
 DataMapper.finalize
+
 DataMapper.auto_upgrade!
